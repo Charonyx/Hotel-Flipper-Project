43527f37091b95c4129565ed0755c1a42418a2a2
Charonyx <churairat.onn@gmail.com>
2022-11-23 01:44:17 +0700

    update dto

--- src/cartuser/dto/createCartuser.dto.ts
+++ src/cartuser/dto/createCartuser.dto.ts
@@ -1,4 +1,3 @@
-import { IsNotEmpty, IsNumber, IsString, Min } from 'class-validator';
 import { CartUserDTO } from './cartuser.dto';
 
 export class CreateCartUserDTO extends CartUserDTO {}
--- src/cartuser/dto/updateCartuser.dto.ts
+++ src/cartuser/dto/updateCartuser.dto.ts
@@ -1,4 +1,3 @@
-import { IsBoolean, IsNotEmpty, IsNumber, Min } from 'class-validator';
 import { CartUserDTO } from './cartuser.dto';
 
 export class UpdateCartUserDTO extends CartUserDTO {}
