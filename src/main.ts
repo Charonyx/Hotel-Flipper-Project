3c902a8830076488652fa123dcdfebdab3ccc123
Charonyx <churairat.onn@gmail.com>
2022-11-23 01:40:08 +0700

    update service

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.setGlobalPrefix('api');
     app.enableCors();
-    await app.listen(4002);
+    await app.listen(4002); // 3000
 }
 bootstrap();
